7e5f0fc66d8eeed2a349d692d40a6ea4256771de
ahmadzaki8828 <ahmadzaki8828@gmail.com>
2024-06-21 21:01:57 -0400

    image carsosel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
+import { ImageSlider } from "@/components/image-slider";
 import { LandingContent } from "@/components/landing-content";
 import { LandingHero } from "@/components/landing-hero";
+import car1 from "./car-1.jpg";
+
+const IMAGES = [
+  { url: "./car-1.jpg", alt: "Car One" },
+  { url: "./car-1.jpg", alt: "Car One" },
+];
 
 const HomePage = () => {
   return (
     <>
       <LandingHero />
       <LandingContent />
+      <ImageSlider images={IMAGES} />
     </>
   );
 };
--- src/components/landing-hero.tsx
+++ src/components/landing-hero.tsx
@@ -7,7 +7,7 @@ export const LandingHero = () => {
   return (
     <div className="text-[#fefae0] font-bold py-10 text-center space-y-5">
       <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
-        <h1>Hi, Welcome to my Protfolio</h1>
+        <h1>Hi, Welcome to my Portfolio</h1>
         <div className="text-transparent bg-clip-text bg-gradient-to-r from-[#fefae0] via-[#606c38] to-[#283618]"></div>
       </div>
       <div className="text-sm md:text-xl font-light text-[#fefae0]">
