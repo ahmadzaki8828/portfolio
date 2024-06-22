6068d1304ec32b1ddd57f34ebb85434241531f3d
ahmadzaki8828 <ahmadzaki8828@gmail.com>
2024-06-21 22:16:05 -0400

    back to homapge

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,10 +1,18 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const ResumePage = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <Image src="/resumeImage.png" width={500} height={500} alt="Resume" />
-    </div>
+    <>
+      <Link href="./">
+        <p className="text-[#f07167] text-md text-center pt-5">
+          Click here to go to back
+        </p>
+      </Link>
+      <div className="flex items-center justify-center min-h-screen">
+        <Image src="/resumeImage.png" width={500} height={500} alt="Resume" />
+      </div>
+    </>
   );
 };
 
