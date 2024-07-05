93840605dd6dc2764e6bf10612c3896cd14f7416
ahmadzaki8828 <ahmadzaki8828@gmail.com>
2024-07-04 22:48:13 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { LandingContent } from "@/components/landing-content";
 import { LandingHero } from "@/components/landing-hero";
 import { MyContact } from "@/components/My-Contact";
-import { ProjectContent } from "@/components/Project-Content";
+import { ProjectContent } from "@/components/project-content";
 
 const HomePage = () => {
   return (
